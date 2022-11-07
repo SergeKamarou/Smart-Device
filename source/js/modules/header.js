365d17cffbcfb33055a91e6aaae159afbea459ac
Комаров Сергей <serkomarov88@gmail.com>
2022-11-07 18:05:40 +0300

    правки после тестирования

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,23 +1,33 @@
 const headerContainerElement = document.querySelector('[data-page-header]');
 const headerElement = headerContainerElement.querySelector('.page-header__wrap');
 
+const MEDIA_DESKTOP_BREAKPOINT = '(max-width: 1023px)';
+
+const breakpoint = window.matchMedia(MEDIA_DESKTOP_BREAKPOINT);
+
 const scrollPos = 1;
 const headerHeight = headerElement.offsetHeight;
 
 const initStickyHeader = () => {
   if (headerContainerElement) {
-    if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos) {
-      headerElement.classList.add('is-sticky');
-      headerContainerElement.style.paddingBottom = headerHeight + 'px';
+    if (!breakpoint.matches) {
+      if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos) {
+        headerElement.classList.add('is-sticky');
+        headerContainerElement.style.paddingBottom = headerHeight + 'px';
+      } else {
+        headerElement.classList.remove('is-sticky');
+        headerContainerElement.style.paddingBottom = 0;
+      }
+
+      window.onscroll = function () {
+        initStickyHeader();
+      };
     } else {
       headerElement.classList.remove('is-sticky');
       headerContainerElement.style.paddingBottom = 0;
     }
-
-    window.onscroll = function () {
-      initStickyHeader();
-    };
   }
+  breakpoint.addEventListener('change', initStickyHeader);
 };
 
 export {initStickyHeader};
